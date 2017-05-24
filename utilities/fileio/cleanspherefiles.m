1c8409c7a4461ff721ff056e8784382d2cfbb2bb
Marc van Wanrooij <m.vanwanrooij@donders.ru.nl>
2017-05-24 12:01:27 +0200

    get rid of graphics handles in .sphere files

---
+++ utilities/fileio/cleanspherefiles.m
@@ -0,0 +1,40 @@
+function cleanspherefiles(dname,EI)
+% Remove graphic handles from sphere-files
+
+if nargin<1
+	dname = cd;
+end
+cd(dname)
+
+if nargin<2
+	d = dir('*-*-*-*');
+	fname = d(1).name;
+	EI = fname(1:2);
+end
+
+d = dir([EI '-*']);
+ndir = numel(d);
+for dIdx = 1:ndir
+	dname = d(dIdx).name;
+	cd(dname)
+	cd('trial')
+	f = dir([EI '-*.sphere']);
+	nfiles = numel(f);
+	for fIdx = 1:nfiles
+		fname = f(fIdx).name;
+		load(fname,'-mat')
+		if isfield(cfg,'hcurtar')
+			disp(fname)
+			cfg = rmfield(cfg,'hcurtar');
+		end
+		if isfield(cfg,'hcurdat')
+			disp(fname)
+			cfg = rmfield(cfg,'hcurdat');
+		end
+		save(fname,'data','cfg','-mat');
+		% 	whos
+	end
+	
+	cd ..
+	cd ..
+end
