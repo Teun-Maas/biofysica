d24326d8cd816b06b486292cd369d781f00add72
Günter Windau <gunter@science.ru.nl>
2019-08-22 16:45:35 +0200

    more rz6_tq stuff

---
+++ experiment/rz6_unircx/rz6_dummy.m
@@ -0,0 +1,24 @@
+classdef r6z_dummy < handle
+
+    methods
+        function ConnectRZ6(this, str, num)
+           disp('ConnectRZ6:');
+           disp(varargin); 
+        end
+
+        function LoadCOF(this,filename)
+           disp('LoadCOF:');
+           disp(varargin); 
+        end
+
+        function WriteTagVEX(this, varargin)
+           disp('WriteTagVEX:');
+           disp(varargin); 
+        end
+
+        function ReadTagVEX(this, varargin)
+           disp('ReadTagVEX:');
+           disp(varargin); 
+        end
+    end
+end
--- experiment/rz6_unircx/rz6_unircx_processor.m
+++ experiment/rz6_unircx/rz6_unircx_processor.m
@@ -16,31 +16,65 @@ classdef rz6_unircx_processor < handle
            delete(this.zBus);
         end
 
-        function upload_tasklist(this, tasklist)
+        function write_tasklist(this, tasklist)
             x=tasklist.get();
             this.write('STM_Matrix',x);
+        end
+
+        functions write_wavdata(this, data, chanlist)
+            for i=1:length(chanlist)
+               chan=chanlist(i);
+               nsamp=size(data,2);
+               sizetag=sprintf('BufferSize%d',chan);
+               datatag=sprintf('WavData%d',chan);
+               this.write(sizetag,nsamp);
+               this.write(datatag,data(chan,:));
+            end
+        end
+
+
+        function read_acqdata(this, chanlist)
+            r=cell(1,length(chanlist));
+            for i=1:length(chanlist)
+               chan=chanlist(i);
+               sizetag=sprintf('Acq%d_Size',chan);
+               datatag=sprintf('Acq%d_Data',chan);
+               szi=this.read(sizetag);
+               r{i}=this.read(datatag,0,szi);
+            end
+        end
+
+        function r=read_acqready(this, chanlist)
+            r=zeros(1,length(chanlist));
+            for i=1:length(chanlist)
+               chan=chanlist(i);
+               readytag=sprintf('Acq%d_Ready',chan);
+               r(i)=this.read(readytag);
+            end
         end
 
         function write(this, tagname, value, offset)
             if nargin < 4
                 offset = 0;
             end
-            if isscalar(value)
-                if ~this.rz6.SetTagVal(tagname,value)
-                    error('SetTagVal');
-                end
-            else
+            %if isscalar(value)
+            %    if ~this.rz6.SetTagVal(tagname,value)
+            %        error('SetTagVal');
+            %    end
+            %else
                 v=reshape(value,[],1);
                 if ~this.rz6.WriteTagVEX(tagname, offset, 'I32', v)
                    error('WriteTagVEX');
                 end
-            end
-
+            %end
         end
 
-        function data=read(this, tagname, nWords, offset, nChannels)
-            if nargin < 4
+        function data=read(this, tagname, offset, nWords, nChannels)
+            if nargin < 3
                 offset = 0;
+            end
+            if nargin < 4
+                nWords = 1;
             end
             if nargin < 5
                 nChannels=1;
