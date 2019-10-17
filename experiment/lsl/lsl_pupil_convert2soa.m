2f46a52e364f5f2f286dbca5acc4020b85702c00
Günter Windau <gunter@science.ru.nl>
2019-10-17 14:40:08 +0200

    changed calling convention of lsl_pupil_convert2soa.m

--- experiment/lsl/lsl_pupil_convert2soa.m
+++ experiment/lsl/lsl_pupil_convert2soa.m
@@ -1,5 +1,9 @@
-function soadata=lsl_pupil_convert2soa(pupil_data)
+function soadata_do_not_use=lsl_pupil_convert2soa(pupil_data)
 %LSL_PUPIL_CONVERT2SOA -  convert lsl pupil-labs data to structure of
 %array type data
-    soadata=aos2soa(lsl_pupil_pyrepr2mat(pupil_data.Data));
+    if nargout > 0
+       error('sorry, calling syntax changed');
+    end
+    %%FIXME pupil_data.pythonData=pupil_data.Data;
+    pupil_data.Data = aos2soa(lsl_pupil_pyrepr2mat(pupil_data.Data));
 end
