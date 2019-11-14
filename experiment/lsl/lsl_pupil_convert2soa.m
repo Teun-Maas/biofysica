5a2c2c2ebaba87278d01725400a7cdc485dcea20
Günter Windau <gunter@science.ru.nl>
2019-11-14 09:49:56 +0100

    reverted to old calling syntax of lsl_pupil_convert2soa.m

--- experiment/lsl/lsl_pupil_convert2soa.m
+++ experiment/lsl/lsl_pupil_convert2soa.m
@@ -1,9 +1,15 @@
-function soadata_do_not_use=lsl_pupil_convert2soa(pupil_data)
+function soadata=lsl_pupil_convert2soa(pupil_data)
 %LSL_PUPIL_CONVERT2SOA -  convert lsl pupil-labs data to structure of
 %array type data
-    if nargout > 0
-       error('sorry, calling syntax changed');
-    end
-    %%FIXME pupil_data.pythonData=pupil_data.Data;
-    pupil_data.Data = aos2soa(lsl_pupil_pyrepr2mat(pupil_data.Data));
+    soadata=aos2soa(lsl_pupil_pyrepr2mat(pupil_data.Data));
 end
+
+%function soadata_do_not_use=lsl_pupil_convert2soa(pupil_data)
+%%LSL_PUPIL_CONVERT2SOA -  convert lsl pupil-labs data to structure of
+%%array type data
+%    if nargout > 0
+%       error('sorry, calling syntax changed');
+%    end
+%    %%FIXME pupil_data.pythonData=pupil_data.Data;
+%    pupil_data.Data = aos2soa(lsl_pupil_pyrepr2mat(pupil_data.Data));
+%end
