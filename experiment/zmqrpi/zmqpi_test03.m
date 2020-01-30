f9f2361f1f18bdaf4c2b57b76674264d44ce0ec5
Günter Windau <gunter@science.ru.nl>
2020-01-30 13:03:49 +0100

    add raspi lsl trigger stuff for Helena

---
+++ experiment/zmqrpi/zmqpi_test03.m
@@ -0,0 +1,72 @@
+function [trigdata, evdata] = zmqpi_test03
+
+  trig=zmqrpi_remote_control('lsldert01.local',5555);
+
+  info=lsl_resolver('type=''Digital Triggers @ lsldert01''');
+  l=info.list();
+  if isempty(l)
+      error('no streams found');
+  end
+   
+  for i=1:numel(l)
+     fprintf('%d: name: ''%s'' type: ''%s''\n',i,l(i).name,l(i).type); 
+  end
+  
+  n=1; %input('enter stream number to acquire: ');
+  
+  trigstr=lsl_istream(info{n});
+
+  info=lsl_resolver('type=''Digital Events @ lsldert01''');
+  l=info.list();
+  if isempty(l)
+      error('no streams found');
+  end
+   
+  for i=1:numel(l)
+     fprintf('%d: name: ''%s'' type: ''%s''\n',i,l(i).name,l(i).type); 
+  end
+  
+  n=1 ; %=input('enter stream number to acquire: ');
+  
+  evstr=lsl_istream(info{n});
+
+  ses=lsl_session();
+  ses.add_stream(trigstr);
+  addlistener(evstr,'DataAvailable',@triglistener);
+
+  ses.add_stream(evstr);
+  addlistener(evstr,'DataAvailable',@evlistener);
+
+  input('press enter to start');
+
+  ses.start();
+  
+  for dt = [ 1 2 3 1 2 1 ]
+     pause(dt);
+     fprintf('playing beep\n');
+     trig.beep();
+  end
+  pause(3);  
+
+  input('press enter to stop');
+  delete(trig);
+  ses.stop();
+  trigdata=trigstr.read();
+  evdata=evstr.read();
+
+  delete(ses);
+  delete(trigstr);
+  delete(evstr);
+  delete(info)
+
+end
+
+function triglistener(src, event)
+   disp('trigger data received');
+   event
+end
+
+function evlistener(src, event)
+   disp('event data received');
+   event
+end
