21eff408240d630b29a72dbb74bcdad74b809c0b
Günter Windau <gunter@zoot.local>
2020-04-21 10:41:17 +0200

    added lslhi_test02.m remote keyboard example

--- experiment/zmqrpi/lslder_kbd_client.m
+++ experiment/zmqrpi/lslder_kbd_client.m
@@ -4,7 +4,26 @@ classdef lslder_kbd_client < lsldert_zmq_subscriber
         function this=lslder_kbd_client(hostname,port)
             this@lsldert_zmq_subscriber(hostname,port);
             this.socket.subscribe('EV_KEY');
-         end
+        end
+         
+        function [key, value] = getkey(this,dontwait)
+            if dontwait
+                str = this.recv_str(1);
+            else
+                str = [];
+                while isempty(str)
+                    str = this.recv_str(0);                    
+                end
+            end
+            if isempty(str)
+                key='';
+                value=NaN;
+                return
+            end
+            ss=strsplit(string(str));
+            key=ss{2};
+            value=str2double(ss{3});
+        end
     end  
     
 end
--- experiment/zmqrpi/lslhid_test02.m
+++ experiment/zmqrpi/lslhid_test02.m
@@ -3,13 +3,8 @@ function lslhid_test02
     client=lslder_kbd_client('raspi4',5558);
     
     for i=1:10
-       % pause(1);
-        s=client.recv_str(0);
-        if isempty(s)
-            s="nothing";
-        end
-        s
-        fprintf("""%s\n""",s);
+        [key,value]=client.getkey(0);
+        fprintf("""%s""\t%d\n",key,value);
     end
     
     delete(client);
