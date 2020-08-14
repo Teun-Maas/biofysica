a8a01381fe8865b57a327542a72e43a16e69c573
Günter Windau <gunter@zoot.local>
2020-08-14 17:17:08 +0200

    more zmqaudio debugging

--- experiment/zmqrpi/lsldert_audio01.m
+++ experiment/zmqrpi/lsldert_audio01.m
@@ -8,15 +8,16 @@ client=lsldert_pubclient('raspi6');
 % pcm_data=single(sin(2*pi*fsine*times));
 
 load handel
-y2=[y',y'];
-y=reshape(y2,[],1);
+y2=[y';y'];
+pcm_data=reshape(y2,1,[]);
 nchan=2;
-nsamp=0.5*max(size(y));
-pcm_data=y;
+nsamp=0.5*max(size(pcm_data));
 pcm_header=uint32([Fs, nchan, nsamp, 0, 0, 0]);
-
-pcm_left=pcm_data';
+pcm_data(:)=0;
 [~,telapsed]=client.send('AF 3', pcm_header, single(pcm_data));
 [~,telapsed]=client.send('AP 3');
 telapsed
+pause(4);
+[~,telapsed]=client.send('AS 3');
+
 delete(client);
