3d7a247d639007879844afd660e5920470ae2f99
Marc van Wanrooij <marcvanwanrooij@gmail.com>
2020-10-14 10:18:58 +0200

    Remove some third-party functions

--- utilities/statistics/ellipse.m
+++ utilities/statistics/ellipse.m
@@ -4,12 +4,12 @@ function [MU,SD,A,x,y] = ellipse(x,y,varargin)
 %  Determine main axes (direction and std) of 2-dimensional matrix [X,Y].
 %
 %
-% PA_ELLIPSE(...,'PARAM1',val1,'PARAM2',val2) specifies optional
+% ELLIPSE(...,'PARAM1',val1,'PARAM2',val2) specifies optional
 % name/value pairs. Parameters are:
 %	'outlier'	- removes outliers, that are 'outlier'*standard deviation
 %	away from the cardinal axis means.
 %
-% See also PA_ELLIPSEPLOT, EIG, COV
+% See also ELLIPSEPLOT, EIG, COV
 
 % 2011  Marc van Wanrooij
 % e-mail: marcvanwanrooij@neural-code.com
@@ -24,12 +24,11 @@ end
 [Veig,Deig]     = eig(cov(x,y)); % Veig gives main axes
 A               = bf_rad2deg(atan2(Veig(2),Veig(1))); % angle
 
-
 %% Delete response >3SD
 if ~isempty(out)
-	SD              = sqrt(diag(Deig)); % diagonal Deig = variance in the 2 main axes
-	xmu             = mean(x);
-	ymu             = mean(y);
+	SD			= sqrt(diag(Deig)); % diagonal Deig = variance in the 2 main axes
+	xmu			= mean(x);
+	ymu			= mean(y);
 	
 	[Xr,Yr]		= rotate2d(x,y,-A);
 	seld		= abs(Xr-mean(Xr))<out*SD(2) & abs(Yr-mean(Yr))<out*SD(1);
@@ -37,8 +36,8 @@ if ~isempty(out)
 	y			= y(seld);
 	
 	%% Eigen-values for covariance-matrix
-	[Veig,Deig]     = eig(cov(x,y)); % Veig gives main axes
-	A               = rad2deg(atan2(Veig(2),Veig(1))); % angle
+	[Veig,Deig] = eig(cov(x,y)); % Veig gives main axes
+	A			= rad2deg(atan2(Veig(2),Veig(1))); % angle
 end
 
 %% diagonal Deig = variance in the 2 main axes
