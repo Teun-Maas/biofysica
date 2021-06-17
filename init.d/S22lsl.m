013c06b27af824c537c88f44557efba85afeffa8
Günter Windau <gunter@science.ru.nl>
2021-06-17 12:03:08 +0200

    add S22lsl.m

---
+++ init.d/S22lsl.m
@@ -0,0 +1,35 @@
+% Set up matlab path for the labstreaminglayer library
+pathdirs = { ...
+   [ biofysica_root '/liblsl/liblsl-Matlab' ]
+   };
+
+for i = 1:size(pathdirs,1)
+   d = pathdirs{i};
+   if exist(d,'dir')
+      p=genpath(d);
+      addpath(p);
+
+      exclude_d=[d '/init.d'];
+      exclude_p=genpath(exclude_d);
+      rmpath(exclude_p);
+
+      exclude_d=[d '/.svn'];
+      exclude_p=genpath(exclude_d);
+      rmpath(exclude_p);
+
+      exclude_d=[d '/.git'];
+      exclude_p=genpath(exclude_d);
+      rmpath(exclude_p);
+
+      exclude_d=[d '/liblsl'];
+      exclude_p=genpath(exclude_d);
+      rmpath(exclude_p);
+
+      % disp(['added directory ', d, ' to the MATLAB path']);
+   else
+      disp(['directory ', d, ' does not exist, not added to the MATLAB path']);
+   end
+   clear pathdirs d p exclude_d i exclude_p
+end
+
+
