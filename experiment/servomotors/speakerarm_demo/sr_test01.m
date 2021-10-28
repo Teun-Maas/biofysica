fb295af06736b32804ad75574a6dc94a96660990
Your Name <you@example.com>
2021-10-28 15:24:35 +0200

    sr_test01

--- experiment/servomotors/speakerarm_demo/sr_test01.m
+++ experiment/servomotors/speakerarm_demo/sr_test01.m
@@ -1,22 +1,67 @@
 
-times=0:0.1:60;
-sv_vert=sinetukey(100, 10, times, 5);
-sv_chair=zeros(size(sv_vert));
-sv_horiz=zeros(size(sv_chair));
-sv_horiz=sv_vert;
+times=0:0.1:20;
+vert=sinetukey(100, 10, times, 1);
+%chair=zeros(size(vert));
+%horiz=zeros(size(chair));
+horiz=vert;
+chair=vert;
 
-plot(times, sv_vert, '.');
-figure(1);
 
 sr=sr_servo;
 
-sr.write_profile(sv_vert, sv_chair, sv_horiz);
+sr.write_profile(vert, chair, horiz);
 sr.enable();
 input('press enter to start')
 sr.start();
 input('press enter to stop')
 sr.stop();
 [pv_vert, pv_chair, pv_horiz]=sr.read_profile_pv();
-figure(2);
-plot(times,pv_vert(1:size(times,2)));
+[sv_vert, sv_chair, sv_horiz]=sr.read_profile_sv();
+
+range=1:length(times);
+
+%% Vertical
+figure('Name','Vert Axis');
+title('SV');
+subplot(131)
+plot(times, vert(range),'.');
+title('SV');
+
+subplot(132);
+plot(times,pv_vert(range),'.');
+title('PV PLC');
+
+subplot(133);
+plot(times,sv_vert(range),'.');
+title('SV PLC');
+
+%% Horizontal
+figure('Name','Horiz Axis');
+title('SV');
+subplot(131)
+plot(times, horiz(range),'.');
+title('SV');
+
+subplot(132);
+plot(times,pv_horiz(range),'.');
+title('PV PLC');
+
+subplot(133);
+plot(times,sv_horiz(range),'.');
+title('SV PLC');
+
+% Seat
+figure('Name','Seat Axis');
+title('SV');
+subplot(131)
+plot(times, chair(range),'.');
+title('SV');
+
+subplot(132);
+plot(times,pv_chair(range),'.');
+title('PV PLC');
+
+subplot(133);
+plot(times,sv_chair(range),'.');
+title('SV PLC');
 delete(sr);
